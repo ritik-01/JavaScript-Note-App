19d772fea37b531012ad40e06a740320423f16cb
ritik <ritik-01>
2020-07-11 19:15:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+console.warn('Note App');
+showNotes();
+
+let addbtn = document.getElementById('addBtn');
+addbtn.addEventListener('click', function (e) {
+    let addTxt = document.getElementById('addTxt');
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    if(addTxt.value.length == 0){
+        return alert("Notes description can't be Null");
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    addTxt.value = '';
+    showNotes();
+});
+
+function showNotes() {
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    let html = "";
+    notesObj.forEach(function(element, index){
+        html += `<div class="noteCard card my-2 mx-4" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Note ${index+1}</h5>
+            <p class="card-text">${element}</p>
+            <button id="${index}" onclick='del(this.id)' href="#" class="btn btn-primary">Delete Note</button>
+        </div>
+    </div>`;
+    });
+    let notesElm = document.getElementById('notes');
+    if(notesObj.length != 0){
+        notesElm.innerHTML = html;
+    }
+    else{
+        notesElm.innerHTML = `<h3 class="text-white">Nothing to Show...Use Add Note Option to add the note</h3>`
+    }
+}
+
+function del(index){
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.splice(index,1);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    showNotes();
+}
+
+let search = document.getElementById('search');
+search.addEventListener('input',function(){
+    let inputVal = search.value.toLowerCase();
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function(element){
+        let cardTxt = element.getElementsByTagName('p')[0].innerText.toLowerCase();
+        if(cardTxt.includes(inputVal)){
+            element.style.display = "block";
+        }
+        else{
+            element.style.display = "none";
+        }
+    })
+})
